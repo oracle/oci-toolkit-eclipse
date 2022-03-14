62847b0f6066b3b4d69d99947e5539c7b276c36b
Jean de Lavarene <jean.de.lavarene@oracle.com>
2022-03-14 18:42:50 +0100

    Revert "Cbateman/add password generation"

--- plugins/src/com/oracle/oci/eclipse/ui/explorer/database/validate/InputValidator.java
+++
@@ -1,7 +0,0 @@
-package com.oracle.oci.eclipse.ui.explorer.database.validate;
-
-import org.eclipse.core.runtime.IStatus;
-
-public abstract class InputValidator<INPUTTYPE> {
-    public abstract IStatus validate(INPUTTYPE inputValue);
-}
